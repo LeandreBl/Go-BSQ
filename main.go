3bc9d13008e6278d6c6b307b712f833d2afb8052
Leandre <leandre.bla@gmail.com>
2021-02-23 11:55:33 +0100

    [dev] finished

--- main.go
+++ main.go
@@ -27,7 +27,8 @@ func getFileContent(filename string) []byte {
 }
 
 func epurSlices(fileSlices [][]byte) [][]byte {
-	epured := [][]byte{}
+	size := len(fileSlices)
+	epured := make([][]byte, 0, size)
 
 	for _, sli := range fileSlices {
 		if sli != nil && len(sli) > 0 {
@@ -57,6 +58,60 @@ func verifySlicesLength(fileSlices [][]byte) bool {
 	return true
 }
 
+func calcValue(ints [][]int, i int, j int) int {
+	if i == 0 || j == 0 {
+		return 1
+	}
+	top := ints[i-1][j]
+	left := ints[i][j-1]
+	topLeft := ints[i-1][j-1]
+	if top <= left && top <= topLeft {
+		return top + 1
+	} else if left <= top && left <= topLeft {
+		return left + 1
+	}
+	return topLeft + 1
+}
+
+func bsq(slices [][]byte) (x int, y int, size int) {
+	ints := make([][]int, len(slices))
+	x = 0
+	y = 0
+	size = 0
+
+	ints[0] = make([]int, len(slices[0]))
+	for i := 0; i < len(ints); i++ {
+		ints[i] = make([]int, len(slices[i]))
+		for j := 0; j < len(ints[i]); j++ {
+			if slices[i][j] == '.' {
+				ints[i][j] = calcValue(ints, i, j)
+			} else {
+				ints[i][j] = 0
+			}
+			if ints[i][j] > size {
+				size = ints[i][j]
+				x = i
+				y = j
+			}
+		}
+	}
+	return x, y, size
+}
+
+func fillXSlice(slices [][]byte, x int, y int, size int) []string {
+	repres := make([]string, len(slices))
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			slices[x-i][y-j] = 'x'
+		}
+	}
+	for i := 0; i < len(slices); i++ {
+		repres[i] = string(slices[i])
+	}
+	return repres
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintln(os.Stderr, "Error: Invalid number of arguments,", len(os.Args), "provided, 1 required")
@@ -81,5 +136,10 @@ func main() {
 	if verifySlicesLength(slices) == false {
 		fmt.Fprintln(os.Stderr, "Error: lines are not all equal in size")
 		os.Exit(84)
+	}
+	x, y, size := bsq(slices)
+	repres := fillXSlice(slices, x, y, size)
+	for _, sli := range repres {
+		fmt.Println(sli)
 	}
 }
